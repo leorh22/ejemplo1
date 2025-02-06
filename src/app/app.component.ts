1f0bf2d37241a0c365c852ef100b346a9798c5ca
ColonelJurten <albertosj.ti21@utsjr.edu.mx>
2025-02-05 21:34:51 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,56 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'fundamentos';
+  //Propiedades
+  nombre = 'Leonardo';
+  email = 'correo.generico@utsjr.edu.mx';
+  aniosServicio = 22;
+  activo = true;
+  alumnos = [24, 25, 26];
+  numero = 6;
+  contador = 0;
+
+  productos = [
+    {
+      id: 1,
+      descripcion: 'Submarinos',
+      precio: 25
+    },
+    {
+      id: 2,
+      descripcion: 'Choco Roles',
+      precio: 20.50
+    },
+    {
+      id: 3,
+      descripcion: 'Rufles de queso',
+      precio: 18
+    },
+  ];
+
+  //Función que muestra si el profesor está activo o no
+  estaActivo(){
+    if(this.activo){
+      return 'Empleado Activo';
+    } else{
+      return 'Empleado Inactivo';
+    }
+  }
+
+  //Funcion que suma los alumnos del arreglo
+  sumarAlumnos(){
+    let suma = 0;
+    for(let i = 0; i < this.alumnos.length; i++){
+      suma += this.alumnos[i];
+    }
+    return suma;
+  }
+//Función que incrementa el contador
+incrementarContador(){
+  this.contador++;
+}
+//Función que decrementa el contador
+decrementarContador(){
+  this.contador--;
+}
 }
